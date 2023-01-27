7d2873b64ca3380632594c4e9d862e65ba5d606c
Nguyen Cuong <31741498+nguyencuong30697@users.noreply.github.com>
2023-01-27 13:16:07 +0700

    Create module VPC

---
+++ main.tf
@@ -0,0 +1,93 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "VPC-Terraform"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet) // Loop list subnet in subnet
+
+  vpc_id            = aws_vpc.vpc.id // Set for vpc id
+  cidr_block        = var.private_subnet[count.index] // Set CIDR
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)] // Set AZ
+
+  tags = {
+    "Name" = "Private-subnet_VPC-Terraform"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet) // Loop list subnet in subnet
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+
+  tags = {
+    "Name" = "Public-subnet_VPC-Terraform"
+  }
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "IGW-Terraform"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    "Name" = "Public-Route-Table_VPC-Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  for_each       = { for k, v in aws_subnet.public_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "public" {
+  depends_on = [aws_internet_gateway.ig]
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "NATGW_VPC-Terraform"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.public.id
+  }
+
+  tags = {
+    "Name" = "Private-Route-Table_VPC-Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public_private" {
+  for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
